2fce98de161f835e3dc807531792aa837d593f8c
Billy Shelton <sheltonbilly@utexas.edu>
2019-03-12 20:31:45 -0500

    starter files have been added to the project.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ export default class App extends Component {
   
   constructor() {
     super();
+    this.state = {
+      gifs: []
+    };
   } 
 
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=vnt9N7HX9ETIpTNcIlM2WU4emVDYwwZw')
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({gifs: responseData.data});
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
   render() { 
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
