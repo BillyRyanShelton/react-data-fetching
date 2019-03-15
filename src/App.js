7c920b07e24b80855d0b07cf4b79016df221424b
Billy Shelton <sheltonbilly@utexas.edu>
2019-03-14 19:05:34 -0500

    changed the background color for body.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
     this.performSearch();
   }
 
-  performSearch = (query = 'cyberpunk') => {
+  performSearch = (query = 'arnold schwarzenegger') => {
     axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit&api_key=vnt9N7HX9ETIpTNcIlM2WU4emVDYwwZw`)    .then(response => {
       this.setState({
         gifs: response.data.data,
